0d53cab129e47f434f96d9243dc4b34488be7f0c
Sarsenov <zhanbek.sarsenov@gmail.com>
2026-03-31 01:21:12 +0500

    Виконує Завдання 7

---
+++ Task_07/cr_t_Cars.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Cars (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    Brand VARCHAR(50),
+    Model VARCHAR(50),
+    EngineVolume DECIMAL(3,1),
+    Price DECIMAL(10,2),
+    MaxSpeed INT
+);
---
+++ Task_07/fill_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO Cars (Brand, Model, EngineVolume, Price, MaxSpeed)
+VALUES
+('Mitsubishi', 'Galant', 2.5, 30000, 210),
+('BMW', 'X5', 3.0, 60000, 240),
+('Chevrolet', 'Cobalt', 2.0, 45000, 230),
+('Hyundai', 'Elantra', 2.0, 50000, 235);
